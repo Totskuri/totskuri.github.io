05b81152214c9ad5db5cbd48be3881318937e869
Joni <herravoima@gmail.com>
2017-08-24 11:40:00 +0300

    scale test

--- MobileTetris/js/preload.js
+++ MobileTetris/js/preload.js
@@ -7,12 +7,13 @@ var preloadState = {
     game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
 
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.parentIsWindow = true;
     game.scale.minWidth = 480;
     game.scale.minHeight = 320;
-    game.scale.maxWidth = 960;
-    game.scale.maxHeight = 640;
-    game.scale.pageAlignHorizontally = true;
-    game.scale.pageAlignVertically = true;
+    game.scale.maxWidth = 1920;
+    game.scale.maxHeight = 1280;
+    //game.scale.pageAlignHorizontally = true;
+    //game.scale.pageAlignVertically = true;
     game.scale.refresh();
     game.state.start('menu');
   }
